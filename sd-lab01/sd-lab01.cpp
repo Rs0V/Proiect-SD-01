7379cf89b684be067dfa1cd7c891e275e21c9eb4
Rs0V <cris.al.iacov@gmail.com>
2023-03-19 00:51:50 +0200

    Corectat/imbunatatit algoritmii.     -de rezolvat quick_sort-ul: duce in stack overflow pt. array-uri mari

--- sd-lab01/sd-lab01.cpp
+++ sd-lab01/sd-lab01.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <chrono>
-#include <time.h>
 #include <random>
+#include <string>
 
 #define FOREX(i, _pair) for(auto i = _pair.first; i < _pair.second; ++i)
 #define FOR(i, _pair) for(auto i = _pair.first; i <= _pair.second; ++i)
 
-#define NPAIR(_pair, f, s, _pair_name) auto _pair_name = std::pair<int, int>(_pair.first + f, _pair.second + s)
+#define RFOREX(i, _pair) for(auto i = _pair.second - 1; i >= _pair.first; --i)
+#define RFOR(i, _pair) for(auto i = _pair.second; i >= _pair.first; --i)
+
+#define NPAIR(_pair, f, s, _new_pair) auto _new_pair = std::pair<int, int>(_pair.first + f, _pair.second + s)
+
+#define SORT_PTR(sort_ptr) void (*sort_ptr)(int*, std::pair<int, int>)
+
+
 
 std::random_device rd;
 std::mt19937 rng(rd());
+
+std::ifstream input("input.in");
+std::ofstream output("output.out");
+
+
 
 int array_max(int* _array, std::pair<int, int> _interval)
 {
@@ -32,42 +45,48 @@ int digit_count(int number)
 	return digits;
 }
 
-int* rand_uint_array(int seed, int array_size)
+int* rand_uint_array(int array_size, int max_nr)
 {
-	srand(seed);
+	std::uniform_int_distribution<std::mt19937::result_type> dist(1, max_nr);
+
 	int* _array = new int[array_size];
 	int i = 0;
 	while (i < array_size)
 	{
-		_array[i] = rand();
+		_array[i] = dist(rng);
 		++i;
 	}
 	return _array;
 }
 
-void insertion_sort(int* _array, std::pair<int, int> _interval)
+int* create_array_copy(int* _array, int array_size)
 {
-	NPAIR(_interval, 1, 0, interv);
-	FOREX(key, interv)
-	{
-		int i = key, j = key - 1;
-		while (_array[i] < _array[j] && j > -1)
-		{
-			_array[i] ^= _array[j], _array[j] ^= _array[i], _array[i] ^= _array[j];
-			--i, --j;
-		}
-	}
+	int* new_array = new int[array_size];
+	for(int i = 0; i < array_size; ++i)
+		new_array[i] = _array[i];
+	return new_array;
 }
 
-void count_sort(int* _array, std::pair<int, int> _interval)
+bool is_sorted(int* _array, std::pair<int, int> _interval)
+{
+	NPAIR(_interval, 1, 0, interv);
+	FOREX(i, interv)
+		if (_array[i - 1] > _array[i])
+			return false;
+	return true;
+}
+
+
+
+void count_sort(int* _array, std::pair<int, int>_interval)
 {
 	int max = array_max(_array, _interval);
 	++max;
 	int* count_array = new int[max] {0};
-	
+
 	FOREX(i, _interval)
 		++count_array[_array[i]];
-	
+
 	int array_index = 0;
 	for (int i = 0; i < max; ++i)
 	{
@@ -81,34 +100,40 @@ void count_sort(int* _array, std::pair<int, int> _interval)
 	delete[] count_array;
 }
 
-void radix_sort_aux(int* _array, std::pair<int, int> _interval, int max, int digit = 1)
-{
-	if (digit < 1)
-		return;
 
-	std::vector<std::vector<int>> freq;
-	freq.resize(10);
+
+void radix_sort_aux(int* _array, std::pair<int, int>_interval, int exp)
+{
+	int* output = new int[_interval.second - _interval.first];
+	int count[10]{ 0 };
+
 	FOREX(i, _interval)
-		freq[_array[i] / digit % 10].push_back(_array[i]);
+		++count[_array[i] / exp % 10];
 
-	int array_index = 0;
-	for (int i = 0; i < 10; ++i)
+	for (int i = 1; i < 10; ++i)
+		count[i] += count[i - 1];
+
+	RFOREX(i, _interval)
 	{
-		for (auto number = freq[i].begin(); number != freq[i].end(); ++number)
-		{
-			_array[array_index] = *number;
-			++array_index;
-		}
+		--count[_array[i] / exp % 10];
+		output[count[_array[i] / exp % 10]] = _array[i];
 	}
-	radix_sort_aux(_array, _interval, max - 1, digit * 10);
+
+	FOREX(i, _interval)
+		_array[i] = output[i];
+
+	delete[] output;
 }
 
-void radix_sort(int* _array, std::pair<int, int> _interval)
+void radix_sort(int* _array, std::pair<int, int>_interval)
 {
-	radix_sort_aux(_array, _interval, digit_count(array_max(_array, _interval)));
+	int max = array_max(_array, _interval);
+	for (int exp = 1; max / exp > 0; exp *= 10)
+		radix_sort_aux(_array, _interval, exp);
 }
 
-// ne mai gandim =))
+
+
 void merge_sort_aux(int* _array, std::pair<int, int> _interval)
 {
 	if (_interval.second - _interval.first > 1)
@@ -116,21 +141,6 @@ void merge_sort_aux(int* _array, std::pair<int, int> _interval)
 		int mid = (_interval.first + _interval.second) / 2;
 		merge_sort_aux(_array, { _interval.first, mid });
 		merge_sort_aux(_array, { mid + 1,_interval.second });
-		
-		/*
-		int i = _interval.first, j = mid + 1, iadd = 1, jadd = 0;
-		while (i < j && j <= _interval.second)
-		{
-			if (_array[i] > _array[j])
-			{
-				_array[i] ^= _array[j], _array[j] ^= _array[i], _array[i] ^= _array[j];
-				i += iadd, j += jadd;
-				iadd ^= jadd, jadd ^= iadd, iadd ^= jadd;
-			}
-			else
-				++i;
-		}
-		*/
 
 		int* aux = new int[_interval.second - _interval.first + 1];
 		{
@@ -159,11 +169,7 @@ void merge_sort_aux(int* _array, std::pair<int, int> _interval)
 		delete[] aux;
 	}
 	else if (_array[_interval.first] > _array[_interval.second])
-	{
-		_array[_interval.first] ^= _array[_interval.second];
-		_array[_interval.second] ^= _array[_interval.first];
-		_array[_interval.first] ^= _array[_interval.second];
-	}
+		std::swap(_array[_interval.first], _array[_interval.second]);
 }
 
 void merge_sort(int* _array, std::pair<int, int> _interval)
@@ -171,45 +177,7 @@ void merge_sort(int* _array, std::pair<int, int> _interval)
 	merge_sort_aux(_array, { _interval.first, _interval.second - 1 });
 }
 
-void quick_sort(int* _array, std::pair<int, int> _interval)
-{
-	if (_interval.second - _interval.first == 1)
-		return;
 
-	std::uniform_int_distribution<std::mt19937::result_type> dist(_interval.first, _interval.second);
-
-	int p[3] = { dist(rng), dist(rng), dist(rng) };
-	int pivot = (p[0] + p[1] + p[2]) / 3;
-	pivot = _array[pivot];
-
-	int* l = new int[_interval.second - _interval.first];
-	int* g = new int[_interval.second - _interval.first];
-	int li = 0, gi = 0;
-	FOREX(i, _interval)
-	{
-		if (_array[i] < pivot)
-			l[li] = _array[i], ++li;
-		else if (_array[i] > pivot)
-			g[gi] = _array[i], ++gi;
-	}
-
-	int k = 0;
-	if (li > 0)
-	{
-		quick_sort(l, { 0, li });
-		for (int i = 0; i < li; ++i)
-			_array[k] = l[i], ++k;
-	}
-	_array[k] = pivot, ++k;
-	if (gi > 0)
-	{
-		quick_sort(g, { 0, gi });
-		for (int i = 0; i < gi; ++i)
-			_array[k] = g[i], ++k;
-	}
-	delete[] l;
-	delete[] g;
-}
 
 void shell_sort(int* _array, std::pair<int, int> _interval)
 {
@@ -227,23 +195,82 @@ void shell_sort(int* _array, std::pair<int, int> _interval)
 	}
 }
 
-int main()
+
+
+int partition(int* _array, int low, int high)
 {
-	int a_len = 8;
-	int* a = rand_uint_array(1, a_len);
+	int pivot = _array[high];
+	int i = low - 1;
+
+	for (int j = low; j <= high - 1; ++j)
+		if (_array[j] < pivot)
+		{
+			++i;
+			i = std::min(i, high);
+			std::swap(_array[i], _array[j]);
+		}
+	std::swap(_array[std::min(i + 1, high)], _array[high]);
 	
-	auto t0 = std::chrono::system_clock::now();
-	shell_sort(a, { 0, a_len });
-	auto t1 = std::chrono::system_clock::now();
-
-	for (int i = 0; i < a_len; ++i)
-		std::printf("%d, ", a[i]);
-	delete[] a;
-
-	std::chrono::duration<double> time = t1 - t0;
-	std::cout << "\n\n" << time.count() << '\n';
-
-	return 0;
+	return i + 1;
 }
 
-// infoarena - pb. AlgSort
+void quick_sort_aux(int* _array, std::pair<int, int> _interval)
+{
+	if (_interval.first < _interval.second) {
+		int pi = partition(_array, _interval.first, _interval.second);
+
+		quick_sort_aux(_array, { _interval.first, pi - 1 });
+		quick_sort_aux(_array, { pi + 1, _interval.second });
+	}
+}
+
+void quick_sort(int* _array, std::pair<int, int> _interval)
+{
+	quick_sort_aux(_array, { _interval.first, _interval.second - 1 });
+}
+
+
+
+int main()
+{
+	int tests, array_size, max_nr;
+	input >> tests;
+
+	for (int i = 0; i < tests; ++i)
+	{
+		input >> array_size >> max_nr;
+		int* a = rand_uint_array(array_size, max_nr);
+
+		SORT_PTR(sorts[]) = { radix_sort, merge_sort, shell_sort, count_sort, quick_sort };
+		std::string sorts_names[] = {"radix_sort", "merge_sort", "shell_sort", "count_sort", "quick_sort" };
+		
+		output << 'T' << i + 1 << ":\n";
+		int nr_sorts = sizeof(sorts) / sizeof(SORT_PTR());
+		for (int j = 0; j < nr_sorts; ++j)
+		{
+			int* a_copy = create_array_copy(a, array_size);
+			
+			auto t0 = std::chrono::system_clock::now();
+			sorts[j](a_copy, { 0, array_size });
+			auto t1 = std::chrono::system_clock::now();
+
+			output << '\t' << sorts_names[j] << ": ";
+			if (is_sorted(a_copy, { 0, array_size }))
+			{
+				std::chrono::duration<double> time = t1 - t0;
+				output << time.count() << "s\n";
+			}
+			else
+				output << "Sort Failed\n";
+
+			std::cout << sorts_names[j] << ": finished!\n\n";
+
+			delete[] a_copy;
+		}
+		output << '\n';
+		std::cout << 'T' << i + 1 << ": finished!\n\n\n\n";
+
+		delete[] a;
+	}
+	return 0;
+}
